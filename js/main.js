bc7be6c9d29c172a89de2e3ebd662f00a12c1133
Ahmed Khaled <ahmed1999khaledaboamer1999@gmail.com>
2024-05-28 00:49:05 +0200

    add dark mode

--- js/main.js
+++ js/main.js
@@ -88,3 +88,128 @@ e.classList.add("active");
 }
  
 
+
+
+
+
+
+
+  //  /*------------------------------------- style switcher--------------------------------*/
+  function styleSwitcherToggle(){
+    let styleSwitcher = document.querySelector(".js-style-switcher"),
+    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+    styleSwitcherToggler.addEventListener("click", function(){
+            styleSwitcher.classList.toggle("open");
+            this.querySelector("i").classList.toggle("fa-times");
+            this.querySelector("i").classList.toggle("fa-cog");
+
+        });
+ }
+styleSwitcherToggle();
+
+  //  /*------------------------------------- theme color--------------------------------*/
+let styleHref = document.querySelector(".change-color"),
+color = document.querySelectorAll(".color");
+// window.onload=function(){
+    
+
+// }
+
+
+let colorStorage = localStorage.getItem("color")
+if(!colorStorage ) {
+    window.localStorage.setItem("color","color-2");
+    styleHref.href = colorStorage;
+}
+
+
+color.forEach((ele) =>{
+      ele.onclick= function () {
+          //1
+          color.forEach((e) =>{
+              e.classList.remove("active");
+          });
+          this.classList.add("active");
+          // 2............
+         let colorTheme = ele.dataset.color;
+           window.localStorage.setItem("color",colorTheme);
+          styleHref.href=window.localStorage.color+".css";
+
+      };
+if(ele.dataset.color === window.localStorage.color){
+   ele.classList.toggle("active");
+
+}
+styleHref.href = window.localStorage.color+(".css");
+
+});
+ 
+
+
+
+
+//    /*------------------------------------- theme color  dark mode--------------------------------*/
+function themeLightDark(){
+  let darkModeCheck=document.querySelector(".js-dark-mode");
+  darkModeCheck.addEventListener("click", function(){
+      if(this.checked){
+  localStorage.setItem("theme-dark","true")
+      }else{
+          localStorage.setItem("theme-dark","false")
+      }
+      themeMode();
+  });
+  function themeMode(){
+      if(localStorage.getItem("theme-dark") === "false"){
+          document.body.classList.remove("t-dark")
+  
+      }else{
+          document.body.classList.add("t-dark")
+  
+      }
+  
+   };
+   if(localStorage.getItem("theme-dark") !== null){
+      themeMode();
+   }
+  if(document.body.classList.contains("t-dark")){
+      darkModeCheck.checked = true;
+  }
+   };
+   themeLightDark()
+
+
+
+   // /*--------------------------------------theme glass effect------------------------------- */
+
+function themeGlassEffect(){
+  let glassEffectChecked =document.querySelector(".js-glass-effect"),
+  glassStyle = document.querySelector(".js-glass-style");
+  glassEffectChecked.addEventListener("click", function(){
+  if(this.checked){
+      localStorage.setItem("glass-effect", "true")
+  }else{
+      localStorage.setItem("glass-effect", "false")
+  
+  }
+  glass();
+  });
+  
+  function glass(){
+      if(localStorage.getItem("glass-effect") === "true"){
+          glassStyle.removeAttribute("disabled")
+      }else{
+          glassStyle.disabled = true;
+  
+      }
+  }
+  if(localStorage.getItem("glass-effect") !== null){
+      glass();
+  }
+  if(!glassStyle.hasAttribute("disabled")){
+      glassEffectChecked.checked= true;
+  }
+  };
+  themeGlassEffect();
+  
+  
